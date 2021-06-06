a8e2fb15b3c0894e0c18a10d4ff64592ce9a0839
Wayne Jhou <jhouwayne@gmail.com>
2021-06-06 23:07:31 +0800

    fix home

--- src/client/app.tsx
+++ src/client/app.tsx
@@ -22,7 +22,7 @@ export const App: React.FC<AppProps> = props => {
             <Router history={history}>
                 <div>
                     <ul>
-                        <li><Link to="/home">Home</Link></li>
+                        <li><Link to="/">Home</Link></li>
                         <li><Link to="/login">Login</Link></li>
                         <li><Link to="/register">Register</Link></li>
                     </ul>
